65e7ac0f1f2c132e6f8179354c770089e982c3e3
Timmy Crawford <timmydcrawford@gmail.com>
2020-12-10 09:54:57 -0800

    phpcs fixes.

--- tests/php/includes/class-wc-tracker-test.php
+++ tests/php/includes/class-wc-tracker-test.php
@@ -10,30 +10,30 @@
  */
 class WC_Tracker_Test extends \WC_Unit_Test_Case {
 
-    /**
-     * Utility method to add filter on woocommerce_admin_disabled
-     */
-    public function __disable_woocommerce_admin() {
-        return true;
-    }
+	/**
+	 * Utility method to add filter on woocommerce_admin_disabled
+	 */
+	public function __disable_woocommerce_admin() {
+		return true;
+	}
 
-    /**
-     * Test the tracking of wc_admin being disabled via filter.
-     */
-    public function test_wc_admin_disabled_get_tracking_data() {
-        $tracking_data = WC_Tracker::get_tracking_data();
+	/**
+	 * Test the tracking of wc_admin being disabled via filter.
+	 */
+	public function test_wc_admin_disabled_get_tracking_data() {
+		$tracking_data = WC_Tracker::get_tracking_data();
 
-        // Test the default case of no filter for set for woocommerce_admin_disabled.
-        $this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data );
-        $this->assertEquals( 'no', $tracking_data['wc_admin_disabled'] );
+		// Test the default case of no filter for set for woocommerce_admin_disabled.
+		$this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data );
+		$this->assertEquals( 'no', $tracking_data['wc_admin_disabled'] );
 
-        // Test the case for woocommerce_admin_disabled filter returning true.
-        add_filter( 'woocommerce_admin_disabled', array( $this, '__disable_woocommerce_admin' ) );
+		// Test the case for woocommerce_admin_disabled filter returning true.
+		add_filter( 'woocommerce_admin_disabled', array( $this, '__disable_woocommerce_admin' ) );
 
-        $tracking_data_disabled_wc_admin = WC_Tracker::get_tracking_data();
-        $this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data_disabled_wc_admin );
-        $this->assertEquals( 'yes', $tracking_data_disabled_wc_admin['wc_admin_disabled'] );
-        remove_filter( 'woocommerce_admin_disabled', array( $this, '__disable_woocommerce_admin' ) );
-    }
+		$tracking_data_disabled_wc_admin = WC_Tracker::get_tracking_data();
+		$this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data_disabled_wc_admin );
+		$this->assertEquals( 'yes', $tracking_data_disabled_wc_admin['wc_admin_disabled'] );
+		remove_filter( 'woocommerce_admin_disabled', array( $this, '__disable_woocommerce_admin' ) );
+	}
 
 }
